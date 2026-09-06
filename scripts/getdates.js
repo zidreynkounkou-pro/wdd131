2873f8dbafdecc5ecc3e1d8382574736a58bcc69
zidreynkounkou-pro <relgazidreynkounkou@gmail.com>
2026-09-05 18:29:58 -0700

    Week01 finished

---
+++ scripts/getdates.js
@@ -0,0 +1,9 @@
+// Get the curret year
+
+const currentYear = document.querySelector('#currentyear');
+
+const currentDate = new Date().getFullYear();
+
+currentYear.textContent = currentDate;
+
+document.querySelector('#lastmodified').textContent = document.lastModified;
