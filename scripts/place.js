adce17a6a6212878f84904d3d92cef2ab25ff3c1
zidreynkounkou-pro <relgazidreynkounkou@gmail.com>
2026-09-24 00:45:03 -0700

    Finished with week 3

---
+++ scripts/place.js
@@ -0,0 +1,49 @@
+const currentYear = document.querySelector('#currentyear');
+
+const currentDate = new Date().getFullYear();
+
+currentYear.textContent = currentDate;
+
+document.querySelector('#lastmodified').textContent = `Last modified: ${document.lastModified}`;
+
+const temperature = document.querySelector('#temperature');
+const conditions = document.querySelector('#conditions');
+const wind = document.querySelector('#wind');
+const windChill = document.querySelector('#windchill');
+const weatherIcon = document.querySelector('#weather');
+const weatherIconImg = document.querySelector('.weather-icon');
+
+const fixtTemperature = 30;
+const fixedWind = 10;
+temperature.textContent = fixtTemperature + '°F';
+wind.textContent = fixedWind + 'mph';
+conditions.textContent = 'Broken clouds';
+const largeScreen = window.matchMedia('(min-width: 748px)');
+
+
+// Icons
+largeScreen.addEventListener('change', () => {
+
+    if (largeScreen.matches) {
+        weatherIcon.textContent = 'Weather 🌥️';
+    }
+    else {
+        weatherIcon.textContent = '';
+        weatherIcon.textContent = 'Weather';
+        weatherIconImg.src = 'images/partly_cloudy.svg';
+        weatherIconImg.alt = 'weather conditions icon';
+    }
+});
+
+
+
+function calculateWindChill(temp, windSpeed) {
+    if (temp <= 50 && windSpeed > 3) {
+        const windChill = 35.74 + 0.6215 * temp - 35.75 * Math.pow(windSpeed, 0.16) + 0.4275 * temp * Math.pow(windSpeed, 0.16);
+
+        return windChill.toFixed(1);
+    }
+    return 'N/A';
+}
+
+windChill.textContent = calculateWindChill(fixtTemperature, fixedWind) + "°F";
